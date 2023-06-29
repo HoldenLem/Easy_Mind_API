3f36723342432c3514f5205c2271ebd443e0db8d
Hanna Lembak <lembakhanna@gmail.com>
2023-06-29 18:42:53 +0300

    Initial commit

---
+++ src/main/java/EaseMood/com/api/Entity/BeckDepressionInventory.java
@@ -0,0 +1,278 @@
+package EaseMood.com.api.Entity;
+
+import java.time.LocalDateTime;
+
+public class BeckDepressionInventory {
+
+    private int id;
+
+    private LocalDateTime createdAt;
+
+    private int result;
+
+    private int questionOne;
+
+    private int questionTwo;
+
+    private int questionThree;
+
+    private int questionFour;
+
+    private int questionFive;
+
+    private int questionSix;
+
+    private int questionSeven;
+    private int questionEight;
+
+    private int questionNine;
+
+    private int questionTen;
+
+    private int questionEleven;
+
+    private int questionTwelve;
+
+    private int questionThirteen;
+
+    private int questionFourteen;
+
+    private int questionFifteen;
+
+    private int questionSixteen;
+
+    private int questionSeventeen;
+
+    private int questionEighteen;
+
+    private int questionNineteen;
+
+    private int questionTwenty;
+
+    private int questionTwentyOne;
+
+    public BeckDepressionInventory() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public int getQuestionOne() {
+        return questionOne;
+    }
+
+    public void setQuestionOne(int questionOne) {
+        this.questionOne = questionOne;
+    }
+
+    public int getQuestionTwo() {
+        return questionTwo;
+    }
+
+    public void setQuestionTwo(int questionTwo) {
+        this.questionTwo = questionTwo;
+    }
+
+    public int getQuestionThree() {
+        return questionThree;
+    }
+
+    public void setQuestionThree(int questionThree) {
+        this.questionThree = questionThree;
+    }
+
+    public int getQuestionFour() {
+        return questionFour;
+    }
+
+    public void setQuestionFour(int questionFour) {
+        this.questionFour = questionFour;
+    }
+
+    public int getQuestionFive() {
+        return questionFive;
+    }
+
+    public void setQuestionFive(int questionFive) {
+        this.questionFive = questionFive;
+    }
+
+    public int getQuestionSix() {
+        return questionSix;
+    }
+
+    public void setQuestionSix(int questionSix) {
+        this.questionSix = questionSix;
+    }
+
+    public int getQuestionSeven() {
+        return questionSeven;
+    }
+
+    public void setQuestionSeven(int questionSeven) {
+        this.questionSeven = questionSeven;
+    }
+
+    public int getQuestionEight() {
+        return questionEight;
+    }
+
+    public void setQuestionEight(int questionEight) {
+        this.questionEight = questionEight;
+    }
+
+    public int getQuestionNine() {
+        return questionNine;
+    }
+
+    public void setQuestionNine(int questionNine) {
+        this.questionNine = questionNine;
+    }
+
+    public int getQuestionTen() {
+        return questionTen;
+    }
+
+    public void setQuestionTen(int questionTen) {
+        this.questionTen = questionTen;
+    }
+
+    public int getQuestionEleven() {
+        return questionEleven;
+    }
+
+    public void setQuestionEleven(int questionEleven) {
+        this.questionEleven = questionEleven;
+    }
+
+    public int getQuestionTwelve() {
+        return questionTwelve;
+    }
+
+    public void setQuestionTwelve(int questionTwelve) {
+        this.questionTwelve = questionTwelve;
+    }
+
+    public int getQuestionThirteen() {
+        return questionThirteen;
+    }
+
+    public void setQuestionThirteen(int questionThirteen) {
+        this.questionThirteen = questionThirteen;
+    }
+
+    public int getQuestionFourteen() {
+        return questionFourteen;
+    }
+
+    public void setQuestionFourteen(int questionFourteen) {
+        this.questionFourteen = questionFourteen;
+    }
+
+    public int getQuestionFifteen() {
+        return questionFifteen;
+    }
+
+    public void setQuestionFifteen(int questionFifteen) {
+        this.questionFifteen = questionFifteen;
+    }
+
+    public int getQuestionSixteen() {
+        return questionSixteen;
+    }
+
+    public void setQuestionSixteen(int questionSixteen) {
+        this.questionSixteen = questionSixteen;
+    }
+
+    public int getQuestionSeventeen() {
+        return questionSeventeen;
+    }
+
+    public void setQuestionSeventeen(int questionSeventeen) {
+        this.questionSeventeen = questionSeventeen;
+    }
+
+    public int getQuestionEighteen() {
+        return questionEighteen;
+    }
+
+    public void setQuestionEighteen(int questionEighteen) {
+        this.questionEighteen = questionEighteen;
+    }
+
+    public int getQuestionNineteen() {
+        return questionNineteen;
+    }
+
+    public void setQuestionNineteen(int questionNineteen) {
+        this.questionNineteen = questionNineteen;
+    }
+
+    public int getQuestionTwenty() {
+        return questionTwenty;
+    }
+
+    public void setQuestionTwenty(int questionTwenty) {
+        this.questionTwenty = questionTwenty;
+    }
+
+    public int getQuestionTwentyOne() {
+        return questionTwentyOne;
+    }
+
+    public void setQuestionTwentyOne(int questionTwentyOne) {
+        this.questionTwentyOne = questionTwentyOne;
+    }
+
+    @Override
+    public String toString() {
+        return "BeckDepressionInventory{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", result=" + result +
+                ", questionOne=" + questionOne +
+                ", questionTwo=" + questionTwo +
+                ", questionThree=" + questionThree +
+                ", questionFour=" + questionFour +
+                ", questionFive=" + questionFive +
+                ", questionSix=" + questionSix +
+                ", questionSeven=" + questionSeven +
+                ", questionEight=" + questionEight +
+                ", questionNine=" + questionNine +
+                ", questionTen=" + questionTen +
+                ", questionEleven=" + questionEleven +
+                ", questionTwelve=" + questionTwelve +
+                ", questionThirteen=" + questionThirteen +
+                ", questionFourteen=" + questionFourteen +
+                ", questionFifteen=" + questionFifteen +
+                ", questionSixteen=" + questionSixteen +
+                ", questionSeventeen=" + questionSeventeen +
+                ", questionEighteen=" + questionEighteen +
+                ", questionNineteen=" + questionNineteen +
+                ", questionTwenty=" + questionTwenty +
+                ", questionTwentyOne=" + questionTwentyOne +
+                '}';
+    }
+}
