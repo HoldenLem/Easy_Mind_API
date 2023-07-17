94885fb1862da6ec595b4102161ae7c491b55c3d
Hanna Lembak <lembakhanna@gmail.com>
2023-07-17 12:40:02 +0300

    sql script added for user_table

---
+++ sql-scripts/user.sql
@@ -0,0 +1,17 @@
+
+\c easy_mind;
+
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS user_table;
+
+CREATE TABLE user_table (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR ( 50 ) NOT NULL,
+  last_name VARCHAR ( 50 ) NOT NULL,
+  password VARCHAR ( 50 ) NOT NULL,
+  email VARCHAR ( 255 ) UNIQUE NOT NULL
+);
