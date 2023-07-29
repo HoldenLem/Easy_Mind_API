d6e8342347d702a93869ef316c837506462691ce
Hanna Lembak <lembakhanna@gmail.com>
2023-07-29 17:24:55 +0300

    replace constructors with annotation @RequiredArgsConstructor

--- src/main/java/EaseMood/com/api/Controller/UserController.java
+++ src/main/java/EaseMood/com/api/Controller/UserController.java
@@ -4,21 +4,19 @@ import EaseMood.com.api.DTO.UserDTO;
 import EaseMood.com.api.Entity.User;
 import EaseMood.com.api.Srvice.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping
     public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDTO) {
