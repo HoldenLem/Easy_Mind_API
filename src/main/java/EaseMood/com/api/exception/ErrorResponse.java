1c644ba5e540e3aba95e9bc05135d6297c320e06
Hanna Lembak <lembakhanna@gmail.com>
2023-07-29 18:24:51 +0300

    //

--- src/main/java/EaseMood/com/api/exception/ErrorResponse.java
+++ src/main/java/EaseMood/com/api/exception/ErrorResponse.java
@@ -2,6 +2,5 @@ package EaseMood.com.api.exception;
 
 import java.util.List;
 public record ErrorResponse(int status, String message, List<String> errors) {
-    // No need to explicitly define constructors, getters, or any other methods
-    // The record automatically generates them for us
+
 }
