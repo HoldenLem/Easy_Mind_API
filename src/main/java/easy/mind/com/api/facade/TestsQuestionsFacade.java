9a7e3166616a01d9cfdff167a88b55ceff76d21a
Hanna Lembak <lembakhanna@gmail.com>
2023-10-06 16:45:55 +0300

    facade completed

---
+++ src/main/java/easy/mind/com/api/facade/TestsQuestionsFacade.java
@@ -0,0 +1,34 @@
+package easy.mind.com.api.facade;
+
+import easy.mind.com.api.DTO.TestsQuestionsDTO;
+import easy.mind.com.api.DTO.conversion.TestQuestionsToDto;
+import easy.mind.com.api.entity.TestsQuestions;
+import easy.mind.com.api.service.TestsQuestionsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TestsQuestionsFacade {
+
+    private final TestsQuestionsService service;
+    public TestsQuestionsDTO create(TestsQuestionsDTO testsQuestionsDTO){
+        return TestQuestionsToDto.convert(service.create(TestQuestionsToDto.convert(testsQuestionsDTO)));
+    }
+
+    public TestsQuestionsDTO getById(long id){
+        return TestQuestionsToDto.convert(service.getById(id));
+    }
+    public void delete(long id){
+         service.delete(id);
+    }
+
+    public List<TestsQuestionsDTO> getAll(){
+        return service.getAll()
+                .stream()
+                .map(TestQuestionsToDto::convert)
+                .toList();
+    }
+}
