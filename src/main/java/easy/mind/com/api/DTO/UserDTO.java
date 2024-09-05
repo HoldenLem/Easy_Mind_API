8bb08ffa2a4d5b9b2a9530d736a3cf352bbc3efb
Romanlembak <lembak.roman@gmail.com>
2024-09-05 23:37:12 +0300

    added new annotations and fixed one annotation for UserDto

--- src/main/java/easy/mind/com/api/DTO/UserDTO.java
+++ src/main/java/easy/mind/com/api/DTO/UserDTO.java
@@ -1,16 +1,17 @@
 package easy.mind.com.api.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import easy.mind.com.api.DTO.annotation.UniqueEmail;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Data
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor
 public class UserDTO {
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private int id;
 
     @NotBlank(message = "firstname cannot be blank")
@@ -20,9 +21,12 @@ public class UserDTO {
     private String lastName;
 
     @NotBlank(message = "password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @NotBlank(message = "password cannot be blank")
     @UniqueEmail(message = "this email is used")
+    @Email(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "this email is invalid")
     private String email;
 }
