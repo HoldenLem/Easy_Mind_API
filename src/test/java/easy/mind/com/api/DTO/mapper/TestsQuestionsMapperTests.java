82dcb42d9e0c05abfd0b46206be25444f87aaa50
Romanlembak <lembak.roman@gmail.com>
2024-09-10 14:09:36 +0300

    added shouldConvertUserToUserDTO() method to UserMapperTests

--- src/test/java/easy/mind/com/api/DTO/mapper/TestsQuestionsMapperTests.java
+++ src/test/java/easy/mind/com/api/DTO/mapper/TestsQuestionsMapperTests.java
@@ -1,8 +1,10 @@
 package easy.mind.com.api.DTO.mapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class TestsQuestionsMapperTests {
     @Autowired
-    TestsQuestionsMapper mapper;
+    private TestsQuestionsMapper mapper;
 }
--- src/test/java/easy/mind/com/api/DTO/mapper/UserMapperTests.java
+++ src/test/java/easy/mind/com/api/DTO/mapper/UserMapperTests.java
@@ -1,8 +1,37 @@
 package easy.mind.com.api.DTO.mapper;
 
+import easy.mind.com.api.DTO.UserDTO;
+import easy.mind.com.api.entity.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
 public class UserMapperTests {
     @Autowired
-    UserMapper userMapper;
+    private UserMapper userMapper;
+
+    @Test
+    public void shouldConvertUserToUserDTO() {
+        User user = User.builder()
+                .id(1)
+                .firstName("Volodymyr")
+                .lastName("Ivanov")
+                .password("secret123")
+                .email("ivanov.volodymyr@gmail.com")
+                .build();
+        UserDTO actualUserDto = userMapper.userToUserDTO(user);
+        UserDTO expectedUserDTO = UserDTO.builder()
+                .id(1)
+                .firstName("Volodymyr")
+                .lastName("Ivanov")
+                .password("secret123")
+                .email("ivanov.volodymyr@gmail.com")
+                .build();
+        assertThat(actualUserDto).isEqualTo(expectedUserDTO);
+    }
 }
