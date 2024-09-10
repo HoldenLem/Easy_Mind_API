ebc03b941b82a1d142c79790ddd8b5d2941f7ebf
Romanlembak <lembak.roman@gmail.com>
2024-09-10 14:42:01 +0300

    added shouldConvertUserDTOtoUser method to UserMapperTests

--- src/test/java/easy/mind/com/api/DTO/mapper/UserMapperTests.java
+++ src/test/java/easy/mind/com/api/DTO/mapper/UserMapperTests.java
@@ -4,8 +4,6 @@ import easy.mind.com.api.DTO.UserDTO;
 import easy.mind.com.api.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.*;
@@ -13,7 +11,7 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
 public class UserMapperTests {
     @Autowired
-    private UserMapper userMapper;
+    private UserMapper mapper;
 
     @Test
     public void shouldConvertUserToUserDTO() {
@@ -24,7 +22,7 @@ public class UserMapperTests {
                 .password("secret123")
                 .email("ivanov.volodymyr@gmail.com")
                 .build();
-        UserDTO actualUserDto = userMapper.userToUserDTO(user);
+        UserDTO actualUserDto = mapper.userToUserDTO(user);
         UserDTO expectedUserDTO = UserDTO.builder()
                 .id(1)
                 .firstName("Volodymyr")
@@ -33,5 +31,25 @@ public class UserMapperTests {
                 .email("ivanov.volodymyr@gmail.com")
                 .build();
         assertThat(actualUserDto).isEqualTo(expectedUserDTO);
+    }
+
+    @Test
+    public void shouldConvertUserDTOtoUser() {
+        UserDTO userDTO= UserDTO.builder()
+                .id(0)
+                .firstName("Natalia")
+                .lastName("Jonson")
+                .password("12345678")
+                .email("jonson.natalia@gmail.edu")
+                .build();
+        User actualUser = mapper.userDTOtoUser(userDTO);
+        User expectedUser = User.builder()
+                .id(0)
+                .firstName("Natalia")
+                .lastName("Jonson")
+                .password("12345678")
+                .email("jonson.natalia@gmail.edu")
+                .build();
+        assertThat(actualUser).isEqualTo(expectedUser);
     }
 }
