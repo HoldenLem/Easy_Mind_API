d4067d2c206248d916e9c24af459481817baae03
Romanlembak <lembak.roman@gmail.com>
2024-09-29 13:04:16 +0300

    refactored TestsAnswers class

--- src/main/java/easy/mind/com/api/entity/TestsAnswers.java
+++ src/main/java/easy/mind/com/api/entity/TestsAnswers.java
@@ -1,10 +1,7 @@
 package easy.mind.com.api.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
@@ -12,24 +9,24 @@ import org.hibernate.type.SqlTypes;
 import java.util.List;
 import java.util.Map;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
 
 @Entity
-@Table(name = "tests_answers")
 public class TestsAnswers extends BaseEntity {
 
-    @Column(name = "tests_id")
-    private int questionsId;
+    @Column(nullable = false)
+    private long testsId;
 
-    @Column(name = "user_id")
-    private int userId;
+    @Column(nullable = false)
+    private long userId;
 
     @JdbcTypeCode(SqlTypes.JSON)
-    @Column(columnDefinition = "jsonb")
-    private List<TestsAnswers.Answers> answer;
+    @Column(columnDefinition = "jsonb", nullable = false)
+    private List<Answers> answer;
 
     @Builder
     public record Answers( Map<Integer, Integer> answers) {
